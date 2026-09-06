72bdb98447306a6e5083506b3f07c36d757d17bf
JulianLeal12 <santiagoleal807@gmail.com>
2026-09-06 18:06:28 -0500

    feat: agregar registro de entradas de inventario (#29)

---
+++ src/models/RegistroInventario/EntradaInventarioService.java
@@ -0,0 +1,31 @@
+package RegistroInventario;
+import conf.ConexionDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class EntradaInventarioService {
+    // Crea un objeto del Repository.
+    //comunicación entre base de datos para guardar la entrada
+    private EntradaInventarioRepository repo = new EntradaInventarioRepository();
+
+    //metodo para registrar
+    public void registrar(EntradaInventario entrada) throws Exception {
+        if (entrada.getCantidad() <= 0) {
+            throw new IllegalArgumentException("La cantidad debe ser mayor a cero");
+        }
+        repo.guardar(entrada); // esto ya incluye el guardado
+        actualizarStock(entrada.getIngredienteId(), entrada.getCantidad());
+    }
+
+    private void actualizarStock(Long ingredienteId, double cantidad) throws Exception {
+        String sql = "UPDATE productos SET stock_actual = stock_actual + ? WHERE producto_id = ?";
+
+        try (Connection conn = ConexionDB.obtenerConexion();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setDouble(1, cantidad);
+            stmt.setLong(2, ingredienteId);
+            stmt.executeUpdate();
+        }
+    }
+}
