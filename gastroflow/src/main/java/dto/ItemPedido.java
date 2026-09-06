656c3fde94d313d1f23e9e40e6c1df436aa0f5e2
Quirogaaaa <gabrielestebanquiroga@gmail.com>
2026-09-06 17:15:17 -0500

    refactor: unificar los dos proyectos en uno solo

---
+++ gastroflow/src/main/java/dto/ItemPedido.java
@@ -0,0 +1,19 @@
+package dto;
+
+import entity.Producto;
+
+import java.math.BigDecimal;
+
+public class ItemPedido {
+    private final Producto producto;
+    private int cantidad = 1;
+
+    public ItemPedido(Producto producto) { this.producto = producto; }
+    public Producto getProducto() { return producto; }
+    public int getCantidad() { return cantidad; }
+    public void aumentarCantidad() { cantidad++; }
+    public void disminuirCantidad() { if (cantidad > 1) cantidad--; }
+    public BigDecimal getSubtotal() {
+        return producto.getPrecioVenta().multiply(BigDecimal.valueOf(cantidad));
+    }
+}
