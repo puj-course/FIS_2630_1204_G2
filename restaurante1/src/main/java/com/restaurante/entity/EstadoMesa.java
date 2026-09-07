8a1e0cc810f79bf2083d188e7dccd3193a8449ea
szml-PUJ <malaverl-sz@javeriana.edu.co>
2026-09-07 00:13:56 -0500

    crear enum estado mesa (#71)

--- restaurante1/src/main/java/com/restaurante/entity/EstadoMesa.java
+++ restaurante1/src/main/java/com/restaurante/entity/EstadoMesa.java
@@ -1,8 +1,42 @@
 package com.restaurante.entity;
 
-public enum EstadoMesa {
-    DISPONIBLE,
-    OCUPADA,
-    RESERVADA,
-    MANTENIMIENTO
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "estados_mesa")
+public class EstadoMesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_estado_mesa")
+    private Long id;
+
+    @Column(name = "codigo_estado", nullable = false)
+    private String codigoEstado;
+
+    @Column
+    private String descripcion;
+
+    public EstadoMesa() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCodigoEstado() {
+        return codigoEstado;
+    }
+
+    public void setCodigoEstado(String codigoEstado) {
+        this.codigoEstado = codigoEstado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
