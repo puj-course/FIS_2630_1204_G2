974f19dd9ad7adf3ca025755986d682bfb5efe1f
Gabriel Quiroga <gabrielestebanquiroga@gmail.com>
2026-09-07 23:09:49 -0500

    Delete restaurante directory

--- restaurante/src/main/java/com/restaurante/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurante.repository;
-
-import com.restaurante.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
