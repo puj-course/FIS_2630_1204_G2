870913e17cade25da76e42008ec012f34204eda7
SAMUEL ZEUDEC MALAVER LEON <samuelzeudec@gmail.com>
2026-09-07 21:17:25 -0500

    Delete restaurante directory

--- restaurante/src/main/java/com/restaurante/repository/PlatoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurante.repository;
-
-import com.restaurante.entity.Plato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatoRepository extends JpaRepository<Plato, Long> {
-}
