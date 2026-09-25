567afe6c2761e019fe2c27f5647032b8ef993c2f
szml-PUJ <malaverl-sz@javeriana.edu.co>
2026-09-24 20:27:05 -0500

    Modificar restauranteApplication (#172)

--- src/main/java/com/restaurante/RestauranteApplication.java
+++ src/main/java/com/restaurante/RestauranteApplication.java
@@ -2,8 +2,18 @@ package com.restaurante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "controller",
+        "service",
+        "repository",
+        "entity",
+        "exceptions",
+})
+@EntityScan(basePackages = "entity")
+@EnableJpaRepositories(basePackages = "repository")
 public class RestauranteApplication {
 
     public static void main(String[] args) {
